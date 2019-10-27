d0939783027941a2b7e3a4e9a2555edf70141d2b
Ada Wan <awan9796@gmail.com>
2019-10-27 17:31:44 -0400

    Successfully calling the following parameters from API and inserted into HTML without styling: city, current temp, windspeed, humidity, high temp, low temp - 5 day forecast next

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
+
+
+
+//api stuff
 var APIKey = "30520a5ce7b5178a150f8c3048451b43";
 
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-"q=Toronto,Canada&units=metric&appid=" + APIKey;
-
-
-
-
+"q=Toronto,Canada&units=metric&appid=" + APIKey
 
 $.ajax({
-url: queryURL,
-method: "GET"
-})
-// We store all of the retrieved data inside of an object called "response"
-.then(function(response) {
+	url: queryURL,
+	method: "GET"
+  })
+  // We store all of the retrieved data inside of an object called "response"
+  .then(function(response) {
 
-    // Log the queryURL
-    console.log(queryURL);
+	// Log the queryURL
+	console.log(queryURL);
 
-    // Log the resulting object
-    console.log(response);
-});
+	// Log the resulting object
+	console.log(response);
+
+	// Transfer content to HTML
+	$(".city").html("<h1>" + response.name + " Current Weather Details</h1>");
+	$(".temperature").text("Current Temperature: " + response.main.temp + " C");
+	$(".windspeed").text("Wind Speed: " + response.wind.speed + "km/h");
+	$(".humidity").text("Humidity: " + response.main.humidity + "%");
+	$(".hiTemp").text("Today's High Temperature: " + response.main.temp_max + " C");
+	$(".lowTemp").text("Today's Low Temperature: " + response.main.temp_min + " C");
+	
+  });
