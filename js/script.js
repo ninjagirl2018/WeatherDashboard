d3e780905e76cda26e0d8dcecfff100d9301e6bc
Ada Wan <awan9796@gmail.com>
2019-11-03 16:22:09 -0500

    App largely working, need to fix bugs for error display clearance, fix media queries, refactor code, add reset search button

--- js/script.js
+++ js/script.js
@@ -1,31 +1,107 @@
+const apiKey = "30520a5ce7b5178a150f8c3048451b43";
 
+  
+  //get windspeed in mph and make sure it displays
+  let mph = (speed) => {
+    return parseFloat(speed * (3600/1609.344)).toFixed(2);
+  } 
+  
+  //Searched cities
+  let searchedCities = [];
+  if( localStorage.getItem("citySearch")){
+    searchedCities = JSON.parse(localStorage.getItem("citySearch"));
+  }
+  
 
+  //JSON method to get the current date
+    let getDate = function(days){
+    let someDate = new Date();
+    let numberOfDaysToAdd = days ;
+    someDate.setDate(someDate.getDate() + numberOfDaysToAdd); 
+  
+    let dd = someDate.getDate();
+    let mm = someDate.getMonth() + 1;
+    let y = someDate.getFullYear();
+  
+    return mm + " / "+ dd + " / "+ y;
+  }
+  
 
-//api stuff
-var APIKey = "30520a5ce7b5178a150f8c3048451b43";
+$(document).ready(function() {
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-"q=Toronto,Canada&units=metric&appid=" + APIKey
+    //Ajax call #1 for the current conditions for the city searched
+    var currentConditions = function(cityName, searched){      
+      $.ajax({url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`, success: function(result){
+      
+      //checks to see if the search value is already in local storage, and if not, will push into local storage
+      if(searched === true){
+        if(searchedCities.includes($("#search input").val()) !== true){
+          searchedCities.push($("#search input").val());
+          localStorage.setItem("citySearch", JSON.stringify(searchedCities));
+        }
+        localStorage.setItem("lastCitySearch", $("#search input").val());
+       }
+    
 
-$.ajax({
-	url: queryURL,
-	method: "GET"
-  })
-  // We store all of the retrieved data inside of an object called "response"
-  .then(function(response) {
+    // AJAX call #2 has to use the response from the Ajax call #1 to pull the object names that are required for the app
+    cityId = result.id;
+        $.ajax({url: `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${apiKey}&units=metric`, 
+        success: function(result){
+          $("#today").html("");
+          $("#today").append(`<div class="blockHeading"><h2>${result.city.name} ( ${getDate(0)} )</h2><img src="https://openweathermap.org/img/w/${result.list[0].weather[0].icon}.png" alt="${result.list[0].weather[0].description}" width='50' height='50'>`);
+          $("#today").append(`<p class="temperature">Temperature: ${result.list[0].main.temp} °C</p>`);
+          $("#today").append(`<p class="humidity"> Humidity: ${result.list[0].main.humidity} %</p>`);
+          $("#today").append(`<p class="wind_speed">Wind Speed:  ${mph(result.list[0].wind.speed)} MPH</p>`);
+          
+          
+    // AJAX call #3 uses the objects that return from the AJAX Call #2 for longitude and latitude to get the UV index
 
-	// Log the queryURL
-	console.log(queryURL);
+        $.ajax({url: `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=` + result.city.coord.lat + "&lon=" + result.city.coord.lon, 
+        success: function(result){
+            $("#today").append(`<p class="uv">UV Index: <span>${result.value}</span></p>` );
+          }});
+         
+    // 5 Day forecast
+          
+        $("#forecast .fiveDays").html("");
+            for(let i=1; i <= 5; i++){
+            let forecast5 = function(i){
+                return('<div>' +
+                '<p class="date">' + getDate(i) + '</p>' +  
+                `<img src="https://openweathermap.org/img/w/${result.list[i].weather[0].icon}.png" alt="${result.list[i].weather[0].description}" "width='50' height='50'>` +
+                `<p class="temperature">Temp: ${result.list[i].main.temp}&nbsp;°C</p>` +
+                `<p class="humidity">Humidity: ${result.list[i].main.humidity}&nbsp;%"</p>` +
+                '</div>');
+                }
+            
+            $("#forecast .fiveDays").append(forecast5(i));
+          }
+        }});
 
-	// Log the resulting object
-	console.log(response);
-
-	// Transfer content to HTML
-	$(".city").html("<h1>" + response.name + " Current Weather Details</h1>");
-	$(".temperature").text("Current Temperature: " + response.main.temp + " C");
-	$(".windspeed").text("Wind Speed: " + response.wind.speed + "km/h");
-	$(".humidity").text("Humidity: " + response.main.humidity + "%");
-	$(".hiTemp").text("Today's High Temperature: " + response.main.temp_max + " C");
-	$(".lowTemp").text("Today's Low Temperature: " + response.main.temp_min + " C");
-	
-  });
+      },error: function () {
+        if ($("#search input").val() === ""){
+          $("#searchError").html("Please pick a city"); 
+        }else{
+          $("#searchError").html("Sorry we have no data on that city");
+        }
+      }
+    });
+}
+      
+      if( localStorage.getItem("lastCitySearch")){
+        currentConditions( localStorage.getItem("lastCitySearch"), false);
+      }else{
+        currentConditions("Toronto", false);
+      }
+    
+      $("#sampleSearches button").on( "click", function() {
+        currentConditions($(this).html().toString(), false);
+      });
+    
+      $("#search button").on( "click", function() {
+        currentConditions($("#search input").val(), true);     
+      });
+    
+      
+    
+      });
